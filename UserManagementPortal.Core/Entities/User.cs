36392549c285300ef3ca7e335edb4ebe2a92dc12
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-16 21:53:10 +0500

    add user

---
+++ UserManagementPortal.Core/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using UserManagementPortal.Core.Enums;
+
+namespace UserManagementPortal.Core.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public UserStatus Status { get; set; } = UserStatus.Unverified;
+    public DateTime LastLoginAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
