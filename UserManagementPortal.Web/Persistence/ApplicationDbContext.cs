426f9e06f5a8f974709f7c9707f79a802e38c3e4
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-16 21:55:21 +0500

    application setup with database context

---
+++ UserManagementPortal.Web/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementPortal.Core.Entities;
+
+namespace UserManagementPortal.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique()
+            .HasDatabaseName("UX_Users_Email");;
+    }
+}
