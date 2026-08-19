1f4974c319ec393bfe49f03b7f728d1e8fa39c40
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-19 20:27:20 +0500

    add email interface and service

--- UserManagementPortal.Core/Interfaces/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserManagementPortal.Core.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendConfirmationEmailAsync(string toEmail, string userId, string confirmationLink);
-    }
-}
