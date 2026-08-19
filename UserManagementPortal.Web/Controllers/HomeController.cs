b6dec2621827c2f3b46080eeefd5c0a4a9bf33fd
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-19 20:39:55 +0500

    add home controller

---
+++ UserManagementPortal.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagementPortal.Models;
+
+namespace UserManagementPortal.Controllers;
+
+[AllowAnonymous]
+public class HomeController : Controller
+{
+    public IActionResult Index() =>
+        User.Identity?.IsAuthenticated == true
+            ? RedirectToAction("Index", "Admin")
+            : RedirectToAction("Login", "Account");
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
