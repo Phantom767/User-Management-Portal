9fbaa6540120a2bbbfd4471557b1fd4fb6a5ef75
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-19 20:38:28 +0500

    update: smtp port renamed to port

--- UserManagementPortal.Core/Models/EmailSettings.cs
+++ UserManagementPortal.Core/Models/EmailSettings.cs
@@ -3,9 +3,8 @@
 public class EmailSettings
 {
     public string SmtpServer { get; set; } = string.Empty;
-    public int SmtpPort { get; set; }
+    public int Port { get; set; }
     public string SenderName { get; set; } = string.Empty;
     public string SenderEmail { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
