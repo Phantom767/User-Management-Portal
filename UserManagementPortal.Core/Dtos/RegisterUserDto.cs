45a880819ede6f434f6bf54dd5e73bd344340945
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-19 20:36:34 +0500

    update: add confirm password

--- UserManagementPortal.Core/Dtos/RegisterUserDto.cs
+++ UserManagementPortal.Core/Dtos/RegisterUserDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace UserManagementPortal.Core.Dtos;
 
-public record RegisterUserDto(string Name, string Email, string Password);
+public record RegisterUserDto(string Name, string Email, string Password, string ConfirmPassword);
