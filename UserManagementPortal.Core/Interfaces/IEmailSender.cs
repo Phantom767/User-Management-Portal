430264e1b7ab1a3803c1fb1dc7511414e2ffd45f
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-19 20:42:16 +0500

    add email sender

---
+++ UserManagementPortal.Core/Interfaces/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace UserManagementPortal.Core.Interfaces;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string email, string subject, string htmlMessage);
+    Task SendConfirmationEmailAsync(string dtoEmail, string confirmLink);
+}
---
+++ UserManagementPortal.Web/Services/EmailSender.cs
@@ -0,0 +1,72 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using UserManagementPortal.Core.Interfaces;
+using UserManagementPortal.Core.Models;
+
+namespace UserManagementPortal.Services;
+
+public class EmailSender(IOptions<EmailSettings> emailSettings, IWebHostEnvironment environment) : IEmailSender
+{
+    private readonly EmailSettings _emailSettings = emailSettings.Value;
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        if (string.IsNullOrWhiteSpace(_emailSettings.SmtpServer))
+        {
+            throw new InvalidOperationException("Настройки SMTP не заданы. Проверьте секцию 'EmailSettings' в appsettings.json.");
+        }
+        
+        var emailMessage = new MimeMessage();
+
+        emailMessage.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.SenderEmail));
+        emailMessage.To.Add(new MailboxAddress("", email));
+        emailMessage.Subject = subject;
+
+        var bodyBuilder = new BodyBuilder { HtmlBody = htmlMessage };
+        emailMessage.Body = bodyBuilder.ToMessageBody();
+
+        using (var client = new SmtpClient())
+        {
+            try
+            {
+                await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, SecureSocketOptions.Auto);
+                    
+                await client.AuthenticateAsync(_emailSettings.SenderEmail, _emailSettings.Password);
+                    
+                await client.SendAsync(emailMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка отправки письма: {ex.Message}");
+                throw; 
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+    
+    public async Task SendConfirmationEmailAsync(
+        string email,
+        string confirmLink)
+    {
+        var templatePath = Path.Combine(
+            environment.ContentRootPath,
+            "EmailTemplates",
+            "ConfirmEmail.html");
+
+        var html = await File.ReadAllTextAsync(templatePath);
+
+        html = html
+            .Replace("{{EMAIL}}", email)
+            .Replace("{{CONFIRM_LINK}}", confirmLink);
+
+        await SendEmailAsync(
+            email,
+            "Подтверждение аккаунта — UserManagementPortal",
+            html);
+    }
+}
