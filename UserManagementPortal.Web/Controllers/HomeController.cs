8ddfa0fe56e42dd542e9664b840e179f81721a22
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-08-20 10:23:54 +0500

    Remove unused using directive in HomeController
    Removed unused 'using UserManagementPortal.Models;' directive.

--- UserManagementPortal.Web/Controllers/HomeController.cs
+++ UserManagementPortal.Web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using UserManagementPortal.Models;
 
 namespace UserManagementPortal.Controllers;
 
